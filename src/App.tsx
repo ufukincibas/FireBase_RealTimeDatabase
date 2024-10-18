286dd2763bcc27d1355bccb9f2a760beba5abb53
ufukincibas <ufukincibas@hotmail.com>
2024-10-18 04:07:27 +0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {View , Text , Button} from "react-native"
+
+function App() {
+  return(
+    <View>
+      <Text>Hello Realtime Database !</Text>
+    </View>
+  )
+}
+
+export default App;
